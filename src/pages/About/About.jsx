d99dba08c271674b2e5584f08d022e1b96177601
Neelesh <sharmaneelesh0607@gmail.com>
2024-07-23 19:00:55 +0530

    completed

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,21 +3,27 @@ import styles from "../About/About.module.css";
 import { Link } from "react-router-dom";
 
 const About = () => {
-  let [select, setSelect] = useState(false);
-  let handleSelect = () => {
-    // setSelect(!select);
+  let handleScroll = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   };
   return (
     <div className={styles.container}>
       <div className={styles.innerContainer}>
         <div className={styles.imgContainer}>
-          <Link className={styles.linkTag} to="/instructor">
+          <Link
+            className={styles.linkTag}
+            onClick={handleScroll}
+            to="/instructor"
+          >
             <img src="about\instructor.png" alt="" />
             INSTRUCTOR
           </Link>
         </div>
         <div className={styles.imgContainer}>
-          <Link className={styles.linkTag} to="/pricing">
+          <Link onClick={handleScroll} className={styles.linkTag} to="/pricing">
             <img src="about\price.png" alt="" />
             PRICING
           </Link>
